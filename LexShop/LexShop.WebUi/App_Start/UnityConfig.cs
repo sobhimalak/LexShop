8f7d92112ccf3c55c6bd244a61139c39ef952ccd
sobhi@cloudulu.com <sobhi@cloudulu.com>
2021-01-13 14:24:27 +0100

    Added SQL Repository

--- LexShop/LexShop.WebUi/App_Start/UnityConfig.cs
+++ LexShop/LexShop.WebUi/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using LexShop.Core.Contracts;
 using LexShop.Core.Models;
 using LexShop.DataAccess.InMemory;
+using LexShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace LexShop.WebUi
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
